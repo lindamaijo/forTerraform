7eff582e30fd35c00a9826223fd6d8d12ebfb1fc
lindamaijo <89674870+lindamaijo@users.noreply.github.com>
2022-02-16 21:49:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "google_compute_network" "vpc_network" {
 name = "terraform-network"
 }
 resource "google_compute_instance" "vm_instance" {
-name = "terraform-instance2"
+name = "terraform-instance3"
 machine_type = "f1-micro"
 zone = "us-central1-c"
 boot_disk {
